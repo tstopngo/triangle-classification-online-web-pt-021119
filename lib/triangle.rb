c828f1e0c1fbe26f2e476dd2e3ce2af60fe3ab6e
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 02:24:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :num1, :num2, :num3
+    
+    def initialize(num1,num2,num3)
+      @num1 = num1
+      @num2 = num2
+      @num3 = num3
+    end
+    
+    def kind
+      if @num1 == 0 && @num2 == 0 && @num3 == 0
+        raise TriangleError
+      elsif @num1.abs != @num1 || @num2.abs != @num2 || @num3.abs != @num3
+        raise TriangleError
+      elsif (@num1 + @num2) <= @num3 || (@num2 + @num3) <= @num1 || (@num1 + @num3) <= @num2
+        raise TriangleError
+      elsif @num1 != @num2 && @num2 != @num3 && @num1 != @num3
+        return :scalene
+      elsif @num1 == @num2 && @num2 == @num3 && @num1 == @num3
+        return :equilateral
+      else
+        return :isosceles 
+      end
+    end
+    
+    class TriangleError < StandardError
+      
+    end
+    
 end

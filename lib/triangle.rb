22e0b13d0f9586e3c39a2aaecdd0df40411db122
Tracy Ngo <tngo1206@gmail.com>
2019-03-25 02:17:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,7 @@ class Triangle
       if @num1 == 0 && @num2 == 0 && @num3 == 0
         raise TriangleError
       elsif @num1.abs != @num1 || @num2.abs != @num2 || @num3.abs != @num3
+        raise Triangle Error
       elsif @num1 != @num2 && @num2 != @num3 && @num1 != @num3
         return :scalene
       elsif @num1 == @num2 && @num2 == @num3 && @num1 == @num3
